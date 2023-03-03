27a192dabec2ac7d3bd58f8360720b11bd8a18d7
Bryan Stopp <bryan.stopp@gmail.com>
2023-03-03 14:09:24 -0600

    Finish hero autoblock.

---
+++ blocks/hero/hero.js
@@ -0,0 +1,15 @@
+/**
+ * decorates the hero,
+ * @param {Element} block The header block element
+ */
+export default async function decorate(block) {
+  block.querySelector('div').classList.add('hero-heading');
+
+  const image = document.createElement('div');
+  image.classList.add('hero-image');
+  image.append(block.querySelector('picture'));
+  const angle = document.createElement('div');
+  angle.classList.add('hero-angle');
+  image.append(angle);
+  block.prepend(image);
+}
