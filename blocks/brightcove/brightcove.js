359b023613a4f832bffbe0bdc68011df1b0d734e
Bryan Stopp <bryan.stopp@gmail.com>
2023-03-06 15:52:19 -0600

    Add video block.

---
+++ blocks/brightcove/brightcove.js
@@ -0,0 +1,12 @@
+export default function decorate(block) {
+  const videoId = block.children[0].children[1].textContent;
+
+  block.innerHTML = `
+    <div class="video-spacer"></div>
+    <div class="video-template">
+      <video-js data-video-id="${videoId}" data-account="940277650001" data-player="BkhQKcpdM" data-embed="default" controls="" data-application-id="" class="vjs-fluid"></video-js>
+    </div>
+  `;
+
+  window.setTimeout(() => import('./brightcove.delayed.js'), 1000);
+}
