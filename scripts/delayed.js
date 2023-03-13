dec0b46d21025a3f999ce857eeb07aea900b74b8
Bryan Stopp <bryan.stopp@gmail.com>
2023-03-13 13:13:17 -0500

    Add GTM call.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,4 +4,15 @@ import { sampleRUM } from './lib-franklin.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-// add more delayed functionality here
+//  Load Google Tag Manager
+const fireGTM = (w, d, s, l, i) => {
+  w[l] = w[l] || [];
+  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+  const f = d.getElementsByTagName(s)[0];
+  const j = d.createElement(s);
+  const dl = l !== 'dataLayer' ? `&l=${l}` : '';
+  j.async = true;
+  j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
+  f.parentNode.insertBefore(j, f);
+};
+fireGTM(window, document, 'script', 'dataLayer', 'GTM-5V9FN8V');
