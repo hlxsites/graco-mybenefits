315dce5db04d937a9c1c5884dcf096f242ef23e6
ekdogan <ekdogan@adobe.com>
2023-03-17 16:17:26 +0100

    webworkerize gtm

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,15 +4,3 @@ import { sampleRUM } from './lib-franklin.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-//  Load Google Tag Manager
-const fireGTM = (w, d, s, l, i) => {
-  w[l] = w[l] || [];
-  w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-  const f = d.getElementsByTagName(s)[0];
-  const j = d.createElement(s);
-  const dl = l !== 'dataLayer' ? `&l=${l}` : '';
-  j.async = true;
-  j.src = `https://www.googletagmanager.com/gtm.js?id=${i}${dl}`;
-  f.parentNode.insertBefore(j, f);
-};
-fireGTM(window, document, 'script', 'dataLayer', 'GTM-5V9FN8V');
